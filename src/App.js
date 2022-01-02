128c0ded8047ed96a7ecd99a4dfb45ed3498e3cc
Pieter Pauwels <r0796064@student.thomasmore.be>
2022-01-02 12:58:26 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+      <ul>
+        <li>All cars</li>
+        <li>Brands by country</li>
+        <li>Models by year</li>
+        <li>Models by type</li>
+      </ul>
+    </nav>
+  );
+};
+
+const Main = () => {
+  return (
+    <div>
+      Content
     </div>
   );
 }
 
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
 export default App;
