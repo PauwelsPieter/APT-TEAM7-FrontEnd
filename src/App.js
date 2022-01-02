c0a1f36dc6f2f323326668a587794a0304c6a5c5
Pieter Pauwels <r0796064@student.thomasmore.be>
2022-01-02 20:22:29 +0100

    Updated navigation styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import './App.css';
 import { Switch, Route, BrowserRouter, NavLink } from 'react-router-dom'; 
 import AllCarsContainer from './containers/all_cars';
@@ -6,14 +7,30 @@ import ModelsByTypeContainer from './containers/models_by_type';
 import ModelsByYearContainer from './containers/models_by_year';
 
 const Header = () => {
+  const [isActive, setActive] = useState(false);
+
+  const handleDisable = () => {
+    setActive(false);
+  }
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact activeClassName="active">All cars</NavLink></li>
-        <li><NavLink to="/brandsbycountry" activeClassName="active">Brands by country</NavLink></li>
-        <li><NavLink to="/modelsbyyear" activeClassName="active">Models by year</NavLink></li>
-        <li><NavLink to="/modelsbytype" activeClassName="active">Models by type</NavLink></li>
+    <nav className='navigation'>
+      <div class="nav-text">APT-Team7 front-end</div>
+      <ul className={isActive ? "active nav-menu" : "nav-menu"}>
+        <li><NavLink to="/" exact activeClassName="active" onClick={handleDisable}>All cars</NavLink></li>
+        <li><NavLink to="/brandsbycountry" activeClassName="active" onClick={handleDisable}>Brands by country</NavLink></li>
+        <li><NavLink to="/modelsbyyear" activeClassName="active" onClick={handleDisable}>Models by year</NavLink></li>
+        <li><NavLink to="/modelsbytype" activeClassName="active" onClick={handleDisable}>Models by type</NavLink></li>
       </ul>
+      <div className="nav-toggle" onClick={handleToggle}>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </div>
     </nav>
   );
 };
